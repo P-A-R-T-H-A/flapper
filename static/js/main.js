f895e379871d51777b383f5113975d5f3742eb83
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-01-19 08:43:34 +0600

    CSS change on load

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,14 @@
 function init() {
-    console.log("Hello Worldf");
-
     const toggleButton = document.getElementById('theme-toggle');
-    const root = document.documentElement;
-    console.log("toggleButton =", toggleButton);
+    const r = document.querySelector(':root');
     toggleButton.addEventListener('click', () => {
-        console.log("Clicked");
-        console.log("Here with", root.style.getPropertyValue('--primary-color'));
-      if (root.style.getPropertyValue('--primary-color') === '#fff') {
-        console.log("Here", root.style.getPropertyValue('--primary-color'));
-        root.style.setProperty('--primary-color', '#333'); 
+      let rs = getComputedStyle(r);
+      if (rs.getPropertyValue('--primary-color') === '#fff') {
+        r.style.setProperty('--primary-color', '#333'); 
       } else {
-        console.log("There");
-        root.style.setProperty('--primary-color', '#fff'); 
+        r.style.setProperty('--primary-color', '#fff'); 
       }
     });
-
 }
 
 document.addEventListener("DOMContentLoaded", init);
